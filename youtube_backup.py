b2ae2aa4d108c92e942a9ba2f1903207fc738664
Dylan Lloyd <dylan@psu.edu>
2010-12-19 21:22:50 -0500

    Refactored and added notifications
    Got rid of the .favorites file in favor of calling youtube-dl in a loop. This allows for notifications to be assembled after each successful download.
    
    Notifications are built and issued using pynotify. Thumbnail images are pulled from Youtube links.
    
    Added extra instructions and an example for setting up this script as a cronjob.
    
    Added myself to __author__ list.
    
    Noted this work as a derivative release under the 3-clause BSD license.

--- youtube_backup.py
+++ youtube_backup.py
@@ -1,20 +1,26 @@
 # -*- coding: utf-8 -*-
 
 """
-    Script that backs up all the Youtube-videos in a users favorites-list
+    Script that backs up all the Youtube videos in a users favorites list and sends an asynchronous alert after successful downloads.
 
+    Please edit the "Settings" section below before running the script.
 
-    Requires youtube-dl to be installed. Use homebrew or follow instruction here: 
-    http://bitbucket.org/rg3/youtube-dl/wiki/Home
+    Requires youtube-dl (http://rg3.github.com/youtube-dl/).
+    Notifications require Nofify OSD (https://launchpad.net/notify-osd), which ships with Ubuntu.
 
-    Please edit the "Settings"-section below before running the script.
-
-    See http://www.github.com/hagnas/youtube_backup for more information and the latest version.
+    See http://www.github.com/nospampleasemam/youtube_backup for more information and the latest version.
 
     -------
 
+    This is a derivative work based off the original youtube_backup script written by Henry Hagnäs and available here:
+    https://github.com/hagnas/youtube_backup
+
+    The original source is subject to the following copyright:
+
     Copyright (c) 2010, Henry Hagnäs <henry@hagnas.com>
     All rights reserved.
+
+    The original source and the derived work contained here is subject to the following license:
 
     Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 
@@ -26,9 +32,21 @@
 
 """
 
-__author__ = "Henry Hagnäs <henry@hagnas.com>"
-__copyright__ = "Henry Hagnäs"
+__authors__ = ("Henry Hagnäs <henry@hagnas.com>", "Dylan Lloyd <dylan@psu.edu>")
 __license__ = "BSD"
+
+# Settings
+
+# Edit User and download-directory info. Directory will NOT be
+# automatically created. DEFAULT_ICON will display in notifications if
+# the script is unable to fetch the video thumbnail from Youtube.
+USER = 'nospampleasemam'
+DIR = '/home/dylan/youtube_backup/'
+YT_DL = '/usr/bin/youtube-dl' # Path to youtube-dl
+NOTIFICATIONS = True
+DEFAULT_ICON ='/usr/share/icons/gnome/48x48/mimetypes/gnome-mime-application-x-shockwave-flash.png'
+YT_OPT = '--no-progress --ignore-errors --continue --max-quality=22 -o "%(stitle)s---%(id)s.%(ext)s"'
+# END OF SETTINGS
 
 from xml.etree import cElementTree as ET
 import urllib2
@@ -37,21 +55,15 @@ import re
 import copy
 import shlex, subprocess
 
-# Settings
-
-# Edit User and download-directory info. Directory will NOT be automatically created.
-USER = 'hagnas'
-DIR = '/Users/hagge/Files/Online Data Backups/youtube/'
-YT_DL = '/usr/local/bin/youtube-dl'
-YT_OPT = '--quiet --ignore-errors --continue --max-quality=22 -a .favorites -o "%(stitle)s--%(id)s.%(ext)s"'
-# quality 22 = 720p
+import pynotify
+import tempfile
+import string
+import hashlib
 
 def get_urls(startindex):
-    """ Fetches the next 10 video-URL's from startindex of USER's favourites
-
-     modified from example found here: http://stackoverflow.com/questions/1452144/simple-scraping-of-youtube-xml-to-get-a-python-list-of-videos
+    """ Fetches the next 10 video-URL's from startindex of USER's favourites.
+     Modified from example found here: http://stackoverflow.com/questions/1452144/simple-scraping-of-youtube-xml-to-get-a-python-list-of-videos.
     """
-
     results = []
     data = urllib2.urlopen('http://gdata.youtube.com/feeds/api/users/{user}/favorites?max-results=10&start-index={startindex}'.format(user=USER, startindex=startindex))
     tree = ET.parse(data)
@@ -60,11 +72,9 @@ def get_urls(startindex):
         for link in entry.findall(ns + 'link'):
             if link.get('rel') == 'alternate':
                 results.append(link.get('href'))
-
     return results
 
 def get_all_urls():
-
     results = []
     result = ['dummy']
     startindex = 1
@@ -72,79 +82,73 @@ def get_all_urls():
         result = get_urls(startindex)
         results = results + result
         startindex = startindex + 10
-
     return results
 
 def get_video_ids():
-    """ cleans up url_list and returns a list with only the Youtube-video id's"""
-
+    """ Cleans up url_list and returns a list with only the Youtube-video id's. """
     url_list = get_all_urls()
-    
     re_videoid = re.compile('watch\?v=(?P<videoid>.*?)&')
-    
     videolist = {}
-
     for i in url_list:
         t = re_videoid.search(i)
         videolist[t.group('videoid')] = i
-        
-    return videolist
-
 def check_for_existing():
-    """ Checks the download-folder for existing videos with same id and removes from videolist """
-
+    """ Checks the download-folder for existing videos with same id and removes from videolist. """
     videolist = get_video_ids()
-
     filelist = os.listdir(DIR)
-
-
     for video in copy.deepcopy(videolist):
         for files in filelist:
             if re.search(video,files):
                 del videolist[video]
-    
     return videolist
 
-def write_fav_file():
-    """ writes a plain text-file called 'favorites' into DIR. One line per youtube-URL. 
-        This is the format youtube-dl wants for batch-downloads.
-        
-        Returns the count on how many new videos have been written to the download-queue.
-    """
-
-    videolist = check_for_existing()
-
-    try:
-        os.chdir(DIR)
-    except(OSError):
-        print 'Could not open directory!'
-        quit()
-
-    stream = open('.favorites', 'w')
-    
-    for i in videolist:
-        stream.write(videolist[i] + '\n')
-
-    stream.close()
-
-    return len(videolist)
-
-def download_files():
+def download_files(videolist):
     """ Uses subprocess to trigger a download using youtube-dl of the list created earlier. """
-    
-
     os.chdir(DIR)
     args = shlex.split(YT_DL + ' ' + YT_OPT)
-
-    p = subprocess.Popen(args)
-
-    
+    if NOTIFICATIONS: regex = re.compile("\[download\] Destination: (.+)")
+    for item in videolist:
+        if item:
+            thread = subprocess.Popen(args + [item], stdout=subprocess.PIPE)
+            output = thread.stdout.read()
+            if NOTIFICATIONS:
+                video_file = regex.findall(output)
+                if len(video_file) == 0:
+                    break
+                thumbnail = hashlib.md5('file://' + DIR + video_file[0]).hexdigest() + '.png'
+                # Two '/'s instead of three because the path is
+                # absolute; I'm not sure how this'd work on windows.
+                title, sep, vid_id = video_file[0].rpartition('---')
+                title = string.replace(title, '_', ' ')
+                thumbnail = os.path.join(os.path.expanduser('~/.thumbnails/normal'), thumbnail)
+                if not os.path.isfile(thumbnail):
+                    opener = urllib2.build_opener()
+                    try:
+                        page = opener.open('http://img.youtube.com/vi/' + item + '/1.jpg')
+                        thumb = page.read()
+                        # The thumbnail really should be saved to
+                        # ~/.thumbnails/normal (Thumbnail Managing
+                        # Standard)
+                        # [http://jens.triq.net/thumbnail-spec/]
+                        # As others have had problems anyway
+                        # (http://mail.gnome.org/archives/gnome-list/2010-October/msg00009.html)
+                        # I decided not to bother at the moment.
+                        temp = tempfile.NamedTemporaryFile(suffix='.jpg')
+                        temp.write(thumb)
+                        temp.flush()
+                        note = pynotify.Notification(title, 'video downloaded', temp.name)
+                    except:
+                        note = pynotify.Notification(title, 'video downloaded', DEFAULT_ICON)
+                else:
+                    # Generally, this will never happen, because the
+                    # video is a new file.
+                    note = pynotify.Notification(title, 'video downloaded', thumbnail)
+                note.show()
 
 def main():
-    
-    if write_fav_file()>0:
-        download_files()
-    
+    videolist = check_for_existing()
+    if len(videolist)>0:
+        download_files(videolist)
 
 if __name__ == '__main__':
     main()
